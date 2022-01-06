99e023bd84f2a023e7d4713c3921ebc6ec8130e0
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-07 08:42:58 +0900

    Learned weblog devlopment 04

--- node-express-mongodb/weblog/app.js
+++ node-express-mongodb/weblog/app.js
@@ -1,1 +1,14 @@
-console.log(process.env.NODE_ENV)
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.disable('x-powered-by')
+
+app.use(
+  '/public',
+  express.static(__dirname + '/public/' + (process.env.NODE_ENV === 'development' ? 'development' : 'production'))
+)
+
+app.use('/', require('./routes/index.js'))
+
+app.listen(3000)
