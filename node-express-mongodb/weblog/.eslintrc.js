f641c21d9d99602dd505a93db04e21faa5f428fb
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-06 21:20:21 +0900

    Learned weblog devlopment 02

---
+++ node-express-mongodb/weblog/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: ['error', 'none'],
+    quotes: ['error', 'single'],
+    'no-console': ['on'],
+  },
+}
