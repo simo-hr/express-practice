5c08fd72b06be5fec5050e83e9dc9e25c6b8977c
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-09 21:55:49 +0900

    Learned weblog devlopment 13

--- node-express-mongodb/weblog/routes/search.js
+++ node-express-mongodb/weblog/routes/search.js
@@ -1,23 +1,35 @@
 const { CONNECTION_URL, OPTIONS, DATABASE } = require('../config/mongdb.config')
+const { MAX_ITEM_PER_PAGE } = require('../config/app.config').search
 const router = require('express').Router()
 const MongoClient = require('mongodb').MongoClient
 
 router.get('/*', (req, res) => {
   const keyword = req.query.keyword || ''
   const regexp = new RegExp(`.*${keyword}.*`)
+  const page = req.query.page ? parseInt(req.query.page) : 1
+  const query = { $or: [{ title: regexp }, { content: regexp }] }
   MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
     const db = client.db(DATABASE)
 
-    db.collection('posts')
-      .find({
-        $or: [{ title: regexp }, { content: regexp }],
-      })
-      .sort({ published: -1 })
-      .toArray()
-      .then((list) => {
+    Promise.all([
+      db.collection('posts').find(query).count(),
+      db
+        .collection('posts')
+        .find(query)
+        .sort({ published: -1 })
+        .skip((page - 1) * MAX_ITEM_PER_PAGE)
+        .limit(MAX_ITEM_PER_PAGE)
+        .toArray(),
+    ])
+      .then((results) => {
         const data = {
           keyword,
-          list,
+          count: results[0],
+          list: results[1],
+          pagination: {
+            max: Math.ceil(results[0] / MAX_ITEM_PER_PAGE),
+            current: page,
+          },
         }
         res.render('../views/search/list.ejs', data)
       })
