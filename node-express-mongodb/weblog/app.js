aa6da24135084461f12b62c05b349f3518c42d3c
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-10 12:10:29 +0900

    Learned weblog devlopment 15

--- node-express-mongodb/weblog/app.js
+++ node-express-mongodb/weblog/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const systemlogger = require('./lib/log/systemlogger')
 const accesslogger = require('./lib/log/accesslogger')
 
@@ -10,11 +11,16 @@ app.use(
   '/public',
   express.static(__dirname + '/public/' + (process.env.NODE_ENV === 'development' ? 'development' : 'production'))
 )
+
+// 静的ファイルへのアクセスログはいらないのでここに配置
+app.use(accesslogger())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.use('/posts/', require('./routes/posts'))
 app.use('/search/', require('./routes/search'))
 app.use('/account/', require('./routes/account'))
-// 静的ファイルへのアクセスログはいらないのでここに配置
-app.use(accesslogger())
 
 app.use('/', require('./routes/index.js'))
 app.use(systemlogger())
--- node-express-mongodb/weblog/routes/account.js
+++ node-express-mongodb/weblog/routes/account.js
@@ -3,7 +3,30 @@ const { MAX_ITEM_PER_PAGE } = require('../config/app.config').search
 const router = require('express').Router()
 const MongoClient = require('mongodb').MongoClient
 
+const createRegistData = (body) => {
+  const dateTime = new Date()
+  return {
+    url: body.url,
+    published: dateTime,
+    update: dateTime,
+    title: body.title,
+    content: body.content,
+    keywords: (body.keywords || '').split(','),
+    authors: (body.authors || '').split(','),
+  }
+}
+
 router.get('/', (req, res) => {
   res.render('../views/account/index.ejs')
 })
+
+router.get('/posts/regist', (req, res) => {
+  res.render('../views/account/posts/regist-form.ejs')
+})
+
+router.post('/posts/regist/input', (req, res) => {
+  const original = createRegistData(req.body)
+  res.render('../views/account/posts/regist-form.ejs', { original })
+})
+
 module.exports = router
