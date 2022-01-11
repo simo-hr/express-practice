8d303853eb638561dcf6d0e36f3960af516218bb
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-11 17:48:04 +0900

    Learned weblog devlopment 25

--- node-express-mongodb/weblog/app.js
+++ node-express-mongodb/weblog/app.js
@@ -31,11 +31,31 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(flash())
 app.use(...accountControl.initialize())
-app.use('/', require('./routes/index.js'))
-app.use('/posts/', require('./routes/posts.js'))
-app.use('/search/', require('./routes/search.js'))
-app.use('/account/', require('./routes/account.js'))
-app.use('/api/posts', require('./api/posts'))
+
+app.use(
+  '/api',
+  (() => {
+    const router = express.Router()
+    router.use('/posts', require('./api/posts'))
+    return router
+  })()
+)
+
+app.use(
+  '/',
+  (() => {
+    const router = express.Router()
+    router.use((req, res, next) => {
+      res.setHeader('X-Frame-Options', 'SAMEORIGIN')
+      next()
+    })
+    router.use('/posts/', require('./routes/posts.js'))
+    router.use('/search/', require('./routes/search.js'))
+    router.use('/account/', require('./routes/account.js'))
+    router.use('/', require('./routes/index.js'))
+    return router
+  })()
+)
 
 app.use(systemlogger())
 
