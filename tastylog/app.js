cc6ef749672b04f16f07e563ebd16505acbf72f8
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-12 15:19:22 +0900

    Learned tastylog devlopment 03

--- tastylog/app.js
+++ tastylog/app.js
@@ -1,2 +1,11 @@
 const PORT = process.env.PORT
-console.log(PORT)
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`Application listening at ${PORT}`)
+})
