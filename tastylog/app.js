12272793088e93d59e2331df30099d2b5fd22f06
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-12 19:06:16 +0900

    Learned tastylog devlopment 05

--- tastylog/app.js
+++ tastylog/app.js
@@ -1,9 +1,18 @@
 const PORT = process.env.PORT
+const path = require('path')
 const express = require('express')
 const app = express()
+
+// Express settings
 app.set('view engine', 'ejs')
 
+// Static resource rooting
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+// Dynamic resource rooting
 app.use('/', require('./routes/index'))
+
+// Execute web application
 app.listen(PORT, () => {
   console.log(`Application listening at ${PORT}`)
 })
