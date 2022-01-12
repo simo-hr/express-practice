dc966071c72cc80d3eeef6572cde52fd5a146ae9
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-12 19:08:24 +0900

    Learned tastylog devlopment 06

--- tastylog/app.js
+++ tastylog/app.js
@@ -1,12 +1,14 @@
 const PORT = process.env.PORT
 const path = require('path')
 const express = require('express')
+const favicon = require('serve-favicon')
 const app = express()
 
 // Express settings
 app.set('view engine', 'ejs')
 
 // Static resource rooting
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
 // Dynamic resource rooting
