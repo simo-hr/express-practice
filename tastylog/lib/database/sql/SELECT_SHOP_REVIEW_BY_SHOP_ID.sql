995fdaccec540cdf766e89611edde3c2619fd2fb
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-13 14:49:21 +0900

    Learned tastylog devlopment 20

---
+++ tastylog/lib/database/sql/SELECT_SHOP_REVIEW_BY_SHOP_ID.sql
@@ -0,0 +1,13 @@
+SELECT
+    t_review.id,
+    t_user.id as 'user_id',
+    t_user.name as 'user_name',
+    t_review.score,
+    t_review.visit,
+    t_review.post,
+    t_review.description
+FROM
+    t_review
+    LEFT JOIN t_user ON t_review.user_id = t_user.id
+WHERE
+    t_review.shop_id = ?
