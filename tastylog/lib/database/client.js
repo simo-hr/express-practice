18000cc2c4e469d5c0bcf7624d6d0acfee3861d9
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-14 16:57:10 +0900

    Learned tastylog devlopment 25

--- tastylog/lib/database/client.js
+++ tastylog/lib/database/client.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const { sql } = require('@garafu/mysql-fileloader')({ root: path.join(__dirname, './sql') })
 const pool = require('./pool')
+const Transaction = require('./transaction')
 
 const MySQLClient = {
   executeQuery: async (query, values) => {
     const result = await pool.executeQuery(query, values)
     return result
+  },
+  beginTransaction: async () => {
+    const tran = new Transaction()
+    await tran.begin()
+    return tran
   },
 }
 
---
+++ tastylog/lib/database/transaction.js
@@ -0,0 +1,50 @@
+const pool = require('./pool')
+const Transaction = class {
+  constructor(connection) {
+    this.connection = connection
+  }
+  async begin() {
+    if (this.Transaction) {
+      this.connection.release()
+    }
+    this.connection = await pool.getConnection()
+    this.connection.beginTransaction()
+  }
+  async executeQuery(query, values, options = {}) {
+    options = {
+      field: options.fields || false,
+    }
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, values, (err, results, fields) => {
+        if (!err) {
+          resolve(!options.fields ? results : { results, fields })
+        } else {
+          reject(err)
+        }
+      })
+    })
+  }
+  async commit() {
+    return new Promise((resolve, reject) => {
+      this.connection.commit((err) => {
+        this.connection.release()
+        if (!err) {
+          resolve()
+        } else {
+          reject()
+        }
+      })
+    })
+  }
+  async rollback() {
+    return new Promise((resolve, reject) => {
+      this.connection.rollback(() => {
+        this.connection.release()
+        this.connection = null
+        resolve()
+      })
+    })
+  }
+}
+
+module.exports = Transaction
