9c0abea93a53fc1afa11ff244dd841cef708bc7d
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-16 15:47:10 +0900

    Learned tastylog devlopment 30

--- tastylog/routes/account.reviews.js
+++ tastylog/routes/account.reviews.js
@@ -2,13 +2,30 @@ const router = require('express').Router()
 const moment = require('moment')
 const { MySQLClient, sql } = require('../lib/database/client.js')
 const DATE_FORMAT = 'YYYY/MM/DD'
+
+const validateReviewData = (req) => {
+  const body = req.body
+  let isValid = true
+  const error = {}
+
+  if (body.visit && !moment(body.visit, DATE_FORMAT).isValid()) {
+    isValid = false
+    error.visit = '訪問日の日付文字列が不正です。'
+  }
+
+  if (isValid) {
+    return undefined
+  }
+  return error
+}
+
 const createReviewData = (req) => {
   const body = req.body
   let date
   return {
     shopId: req.param.shopId,
     score: parseFloat(body.score),
-    visit: (date = moment(body.visit, DATE_FORMAT)) && date.isValid ? date.toDate() : null,
+    visit: (date = moment(body.visit, DATE_FORMAT)) && date.isValid() ? date.toDate() : null,
     post: new Date(),
     description: body.description,
   }
@@ -34,7 +51,6 @@ router.get('/regist/:shopId(\\d+)', async (req, res, next) => {
   }
 })
 
-
 router.post('/regist/:shopId(\\d+)', async (req, res, next) => {
   const review = createReviewData(req)
   const { shopId, shopName } = req.body
@@ -42,8 +58,15 @@ router.post('/regist/:shopId(\\d+)', async (req, res, next) => {
 })
 
 router.post('/regist/confirm', async (req, res, next) => {
+  const error = validateReviewData(req)
   const review = createReviewData(req)
   const { shopId, shopName } = req.body
+
+  if (error) {
+    console.log(error)
+    res.render('./account/reviews/regist-form.ejs', { error, shopId, shopName, review })
+    return
+  }
   res.render('./account/reviews/regist-confirm.ejs', { shopId, shopName, review })
 })
 
