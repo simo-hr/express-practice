bd7f1e61c2ead4447d89c1851163be5c54855255
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-20 22:05:42 +0900

    Learned tastylog devlopment 41

--- tastylog/routes/account.js
+++ tastylog/routes/account.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const { authenticate, authorize, PRIVILEGE } = require('../lib/security/accesscontrol')
 
+router.get('/', (req, res, next) => {
+  res.render('./account/index.ejs')
+})
+
 router.get('/login', (req, res, next) => {
   res.render('./account/login.ejs', { message: req.flash('message') })
 })
