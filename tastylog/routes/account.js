64f0ecc1d185495d9ee0a52c2b02645f5986f744
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-20 18:36:22 +0900

    Learned tastylog devlopment 37

--- tastylog/routes/account.js
+++ tastylog/routes/account.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.use('/login', (req, res, next) => {
+  res.render('./account/login.ejs')
+})
+
 router.use('/reviews', require('./account.reviews'))
 
 module.exports = router
