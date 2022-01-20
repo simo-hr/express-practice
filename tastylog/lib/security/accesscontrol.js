ff4cd6acb576087b20cc69403a2b5b93c8baad3e
xxbbxx825 <xxbbxx825@gmail.com>
2022-01-20 19:13:44 +0900

    Learned tastylog devlopment 39

--- tastylog/lib/security/accesscontrol.js
+++ tastylog/lib/security/accesscontrol.js
@@ -1,4 +1,47 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const { MySQLClient, sql } = require('../database/client')
+const PRIVILEGE = {
+  NORMAL: 'normal',
+}
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.use(
+  'local-strategy',
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+      passReqToCallback: true,
+    },
+    async (req, username, password, done) => {
+      let results, user
+      try {
+        results = await MySQLClient.executeQuery(await sql('SELECT_USER_BY_EMAIL'), [username])
+      } catch (error) {
+        return done(error)
+      }
+      if (results.length === 1 && password === results[0].password) {
+        user = {
+          id: results[0].id,
+          name: results[0].name,
+          email: results[0].email,
+          permissions: [PRIVILEGE.NORMAL],
+        }
+        done(null, user)
+      } else {
+        done(null, false, req.flash('message', 'ユーザー名 または パスワードが間違っています。'))
+      }
+    }
+  )
+)
 
 const initialize = () => {
   return [
@@ -12,11 +55,19 @@ const initialize = () => {
     },
   ]
 }
-const authenticate = () => {}
+
+const authenticate = () => {
+  return passport.authenticate('local-strategy', {
+    successRedirect: '/account',
+    failureRedirect: '/account/login',
+  })
+}
+
 const authorize = () => {}
 
 module.exports = {
   initialize,
   authenticate,
   authorize,
+  PRIVILEGE,
 }
--- tastylog/routes/account.js
+++ tastylog/routes/account.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
+const { authenticate, authorize, PRIVILEGE } = require('../lib/security/accesscontrol')
 
-router.use('/login', (req, res, next) => {
-  res.render('./account/login.ejs')
+router.get('/login', (req, res, next) => {
+  res.render('./account/login.ejs', { message: req.flash('message') })
 })
+
+router.post('/login', authenticate())
 
 router.use('/reviews', require('./account.reviews'))
 
